3b06df37097d14f9670b84f5207f099df4e01c21
Ari Croock <aricroock@gmail.com>
2016-09-26 22:36:02 +0200

    Added 3rd accel offset state and lots of cleanup

--- heave_sim/kalman.py
+++ heave_sim/kalman.py
@@ -1,24 +1,36 @@
 import numpy as np
 import scipy.linalg
 from filterpy.kalman import KalmanFilter
-from filterpy.common import Q_discrete_white_noise
 
 
-def SinusoidalMotionKalmanFilter(R_std, Q, dt, w, P=100):
-    n_states = 2
+'''
+Constructs a Kalman filter object for tracking sinusoidal motion,
+given an acceleration measurement
+
+R_std - stddev of acceleration measurement
+Q - process noise covariance matrix
+dt - sample time
+w - sinusoid frequency (rad/s)
+x0 - initial state vector
+P - initial covariance matrix
+'''
+def SinusoidalMotionKalmanFilter(R_std, Q, dt, w, x0,
+                                 P=100*np.eye(3)):
+    # states: position, velocity, acceleration offset
+    n_states = 3
     kf = KalmanFilter(dim_x=n_states, dim_z=1)
-    kf.x = np.zeros(n_states)
-    kf.P[0, 0] = P
-    kf.P[1, 1] = P
+    kf.x = x0
+    kf.P = P
+
     kf.R *= R_std**2
-    kf.Q = Q_discrete_white_noise(2, dt, Q)
-    # this approximation actually works better than
-    # Q_discrete_white_noise for some reason...
-    kf.Q = np.array([[0, 0],
-                     [0, Q]])
-    print(kf.Q)
-    A = np.array([[0,     1],
-                  [-w**2, 0]])
+    kf.Q = Q
+
+    # continuous-time state transition matrix
+    A = np.array([[0,     1, 0],
+                  [-w**2, 0, 0],
+                  [0,     0, 0]])
+    # discretize using matrix exponential
     kf.F = scipy.linalg.expm(A * dt)
-    kf.H = np.array([[-w**2, 0]])
+    kf.H = np.array([[-w**2, 0, 1]])
+
     return kf
