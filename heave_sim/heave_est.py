429690fe24589b448557b52d675cfb2ad483049f
Ari Croock <aricroock@gmail.com>
2016-10-17 14:06:12 +0200

    Create Sine Kalman filter class and move a lot of code into it

--- heave_sim/heave_est.py
+++ heave_sim/heave_est.py
@@ -1,115 +1,55 @@
-from sensor import SinusoidalMotion
 from kalman import SinusoidalMotionKalmanFilter
+from kalman import find_mode_params
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
-import peakutils
 from peakutils.plot import plot as pplot
 import math
-import numpy
-from spectrum import spectrum
 
 
 np.random.seed(1233)
 
 # number of sinusoidal modes expected in signal
-num_modes = 3
+num_modes = 1
 
 dt = 1 / 50
-R = 0.01
-q = 0.06
+R = 2e-5
+q = 0.00006
 
 
 zs = []
-# 'temp_data/10-07-heave.csv'
-with open('platform_data/11-10-heave.csv', 'r') as dataFile:
+# 'platform_data/11-10-heave.csv
+with open('platform_data/15-10-heave.csv', 'r') as dataFile:
     reader = csv.reader(dataFile, delimiter=",")
     for l in reader:
-        zs.append(float(l[2]))
+        zs.append(-float(l[2]))
 N = len(zs)
 zs = np.asarray(zs)
 
 """
 Use FFT and peak detection to model the data
 """
-# Need to detrend the data
-acc = zs - np.mean(zs)
-y, p, f = spectrum(acc, 1/dt, len(acc))
-
-# find num_modes peaks in FFT, sorted by magnitude
-y_limited = y
-y_limited[:10] = 0
-indexes = peakutils.peak.indexes(y_limited, thres=0.1, min_dist=np.ceil(0.1/dt/N))
-peaks = sorted(zip(indexes, f[indexes], y[indexes], p[indexes]), key=lambda p: p[2], reverse=True)[:num_modes]
-#indexes, _, _,_ = zip(*peaks)
-#indexes = list(indexes)
-interp_freqs = peakutils.interpolate(f, y_limited, ind=[p[0] for p in peaks])
-interp_peaks = zip(interp_freqs, y[indexes], p[indexes])
-pplot(f, y_limited, [p[0] for p in peaks])
-plt.show()
+modes = find_mode_params(zs, 1/dt, num_modes)
 
 # plot reconstructed signal
+acc = zs - np.mean(zs)
 t = np.linspace(0., (len(acc)-1)*dt, len(acc))
-sig = y[0] * np.ones(len(t))
-for i in indexes[:num_modes]:
-#for i in indexes:
-    sig += y[i] * np.cos(2*np.pi*f[i]*t + p[i])
-    print("f=%f: A=%f, p=%f" % (f[i], y[i], p[i]))
-interp_sig = y[0] * np.ones(len(t))
-for peak in interp_peaks:
-    interp_sig += peak[1] * np.cos(2*np.pi*peak[0]*t + peak[2])
-    print("interp: f=%f: A=%f, p=%f" % (peak[0], peak[1], peak[2]))
+sig = np.zeros(len(t))
+for m in modes:
+    sig += m['y'] * np.cos(2*np.pi*m['f']*t + m['p'])
+    print("f=%f: A=%f, p=%f" % (m['f'], m['y'], m['p']))
 plt.plot(t,sig,t,acc)
 plt.show()
-
-# get initial conditions for each mode
-# num_states = 2 * num_modes + 1
-x0 = []
-ws = []
-for idx in indexes[:num_modes]:
-    fft_motion = SinusoidalMotion(acc_mag=y[idx], f=f[idx], phase=p[idx], dt=dt)
-    # pos and vel for each mode
-    x0_m = [fft_motion.position(0), fft_motion.velocity(0)]
-    x0.extend(x0_m)
-
-    ws.append(2*np.pi*f[idx])
-# add final offset state
-x0.append(np.mean(zs))
-
-print("Initial state:", x0)
-#print("Average acc mag.:", fft_motion.acc_mag)
 
 
 """
 Apply Kalman filter
 """
-# initial covariance matrix
-# there is some uncertainty in the pos and vel
-# but accel offset is fairly constrained
-P = np.eye(2*num_modes + 1) * 10
-P[-1,-1] = 1
-
-# this matrix is basically trial and error (i.e. guessing)
-# diagonals are the process noise variance for each state variable
-# higher frequency modes have a higher process noise variance
-# however the accel offset is not expected to change much
-Q = np.eye(2*num_modes+1)
-Q[range(0, 2*num_modes, 2), range(0, 2*num_modes, 2)] = ((q/10)**2) * np.arange(1., num_modes+1)
-Q[range(1, 2*num_modes, 2), range(1, 2*num_modes, 2)] = (q**2) * np.arange(1., num_modes+1)
-Q[-1, -1] = (0.01)**2
-print('Process Noise (Q):', numpy.array_str(Q, max_line_width=1000000))
-
-kf = SinusoidalMotionKalmanFilter(R, Q, dt, ws, x0, P)
+kf = SinusoidalMotionKalmanFilter(R, q, dt, num_modes, zs)
 xests, variances, ps = [], [], []
 for z in zs:
-    kf.predict()
-    kf.update(z)
-    xests.append(kf.x)
-    # variances
-    variances.append(kf.P.diagonal())
-    ps.append(kf.P)
+    xests.append(kf.step(z))
 xests = np.asarray(xests)
-variances = np.asarray(variances)
 
 
 """
@@ -132,7 +72,7 @@ ax_off.set_title('$x_2[k]$: Acceleration Offset')
 
 plt.show()
 
-print('Kalman Gains:', np.asarray(kf.K))
+print('Kalman Gains:', np.asarray(kf.kf.K))
 
 
 # save the results
--- heave_sim/kalman.py
+++ heave_sim/kalman.py
@@ -1,55 +1,130 @@
 import numpy as np
 import scipy.linalg
 from filterpy.kalman import KalmanFilter
+from sensor import SinusoidalMotion
+from spectrum import spectrum
+import peakutils
 
 
 '''
-Constructs a Kalman filter object for tracking sinusoidal motion,
-given an acceleration measurement
-
-R_std - stddev of acceleration measurement
-Q - process noise covariance matrix
-dt - sample time
-ws - list of sinusoid frequencies (rad/s)
-x0 - initial state vector (pos and vel for each mode, and offset)
-P - initial covariance matrix
+Find the parameters for n_modes sinusoids from the acceleration data in acc
+with sample rate Fs
+Returns: [{i, f, y, p}, ...], where y=magnitude, p=phase
 '''
-def SinusoidalMotionKalmanFilter(R_std, Q, dt, ws, x0, P):
-    # states: (pos and vel for each mode, and offset)
-    n_states = len(x0)
-    n_modes = len(ws)
+def find_mode_params(acc, Fs, n_modes):
+    N = len(acc)
 
-    # n_states = num_modes * 2 + 1
-    assert(n_modes == (n_states-1)//2)
+    # some peak finding parameters
+    thresh = 0.1
+    min_dist = np.ceil(0.1*Fs/N)    # 0.1 Hz
 
-    kf = KalmanFilter(dim_x=n_states, dim_z=1)
-    kf.x = x0
-    kf.P = P
+    # detrend the data before FFT
+    acc = acc - np.mean(acc)
+    y, p, f = spectrum(acc, Fs, N)
 
-    kf.R *= R_std**2
-    kf.Q = Q
+    # ignore the first 10 samples to ignore very low frequency peaks
+    y_limited = y
+    y_limited[:10] = 0
 
-    def state_sub_mat(w):
-        return np.array([[0    , 1],
-                          [-w**2, 0]])
+    # find n_modes peaks in FFT, sorted by magnitude
+    indexes = peakutils.peak.indexes(y_limited, thres=thresh, min_dist=min_dist)
+    peaks = zip(indexes, f[indexes], y[indexes], p[indexes])
+    # convert to dict so elements can't be confused
+    peaks = [dict(zip(('i', 'f', 'y', 'p'), p)) for p in peaks]
+    peaks = sorted(peaks, key=lambda p: p['y'], reverse=True)[:n_modes]
+    # frequency interpolation. may not be necessary
+    #indexes, _, _,_ = zip(*peaks)
+    #indexes = list(indexes)
+    # interp_freqs = peakutils.interpolate(f, y_limited, ind=[p[0] for p in peaks])
+    #interp_peaks = zip(interp_freqs, y[indexes], p[indexes])
 
-    # continuous-time state transition matrix
-    # each mode causes a submatrix as defined in state_sub_mat
-    # to appear along the diagonals of A, surrounded by zeros
-    A = np.zeros((n_states, n_states))
-    for i in range(n_modes):
-        A[2*i:2*i+2, 2*i:2*i+2] = state_sub_mat(ws[i])
-    print(A)
-    # discretize using matrix exponential
-    kf.F = scipy.linalg.expm(A * dt)
+    return peaks
 
-    # construct measurement matrix
-    # Each row represents the state of one mode: [-w**2, 0].
-    # Then the matrix is flattened to the correct size and the offset is appended
-    H = np.transpose(np.vstack((-np.square(ws), np.zeros_like(ws)))).ravel()
-    print(np.transpose(np.vstack((-np.square(ws), np.zeros_like(ws)))).ravel())
-    kf.H = np.append(H, 1.)
-    kf.H = np.asarray([kf.H])
-    print("H: ", (kf.H))
+def get_initial_conditions(modes, acc_train_data, dt):
+    # num_states = 2 * n_modes + 1
+    x0 = []
+    for m in modes:
+        fft_motion = SinusoidalMotion(acc_mag=m['y'], f=m['f'], phase=m['p'], dt=dt)
+        # pos and vel for each mode
+        x0_m = [fft_motion.position(0), fft_motion.velocity(0)]
+        x0.extend(x0_m)
+    # add final offset state
+    x0.append(np.mean(acc_train_data))
 
-    return kf
+    return x0
+
+class SinusoidalMotionKalmanFilter:
+
+    '''
+    Constructs a Kalman filter object for tracking sinusoidal motion,
+    given an acceleration measurement
+
+    r - stddev of acceleration measurement
+    q - process noise stddev
+    dt - sample time
+    n_modes - expected number of sinusoidal modes present in signal
+    acc_train_data - acceleration data used to indentify the sinusoidal mode
+        parameters
+    '''
+    def __init__(self, r, q, dt, n_modes, acc_train_data):
+        self.n_modes = n_modes
+
+        modes = find_mode_params(acc_train_data, 1/dt, n_modes)
+
+        # eigenfrequencies for each mode
+        ws = [2*np.pi*m['f'] for m in modes]
+
+        x0 = get_initial_conditions(modes, acc_train_data, dt)
+
+        # n_states = n_modes * 2 + 1
+        n_states = len(x0)
+        assert(n_modes == (n_states-1)//2)
+
+        kf = KalmanFilter(dim_x=n_states, dim_z=1)
+        kf.x = x0
+
+        # initial covariance matrix
+        # there is some uncertainty in the pos and vel
+        # but accel offset is fairly constrained
+        kf.P = np.eye(2*n_modes + 1) * 10
+        kf.P[-1,-1] = 1
+
+        kf.R *= r**2
+
+        # this matrix is basically trial and error (i.e. guessing)
+        # diagonals are the process noise variance for each state variable
+        # higher frequency modes have a higher process noise variance
+        # however the accel offset is not expected to change much
+        kf.Q = np.eye(2*n_modes+1)
+        kf.Q[range(0, 2*n_modes, 2), range(0, 2*n_modes, 2)] = ((q/10)**2) * np.arange(1., n_modes+1)
+        kf.Q[range(1, 2*n_modes, 2), range(1, 2*n_modes, 2)] = (q**2) * np.arange(1., n_modes+1)
+        kf.Q[-1, -1] = (0.01)**2
+        print('Process Noise (Q):\n', np.array_str(kf.Q, max_line_width=1000000), '\n')
+
+        # continuous-time state transition matrix
+        # each mode causes a submatrix as defined in state_sub_mat
+        # to appear along the diagonals of A, surrounded by zeros
+        def state_sub_mat(w):
+            return np.array([[0    , 1],
+                              [-w**2, 0]])
+        A = np.zeros((n_states, n_states))
+        for i in range(n_modes):
+            A[2*i:2*i+2, 2*i:2*i+2] = state_sub_mat(ws[i])
+        print('Transition matrix (A):\n', np.array_str(A, max_line_width=1000000), '\n')
+        # discretize using matrix exponential
+        kf.F = scipy.linalg.expm(A * dt)
+
+        # construct measurement matrix
+        # Each row represents the state of one mode: [-w**2, 0].
+        # Then the matrix is flattened to the correct size and the offset is appended
+        H = np.transpose(np.vstack((-np.square(ws), np.zeros_like(ws)))).ravel()
+        kf.H = np.append(H, 1.)
+        kf.H = np.asarray([kf.H])
+        print('Measurement matrix (H):\n', np.array_str(kf.H, max_line_width=1000000), '\n')
+
+        self.kf = kf
+
+    def step(self, z):
+        self.kf.predict()
+        self.kf.update(z)
+        return self.kf.x
