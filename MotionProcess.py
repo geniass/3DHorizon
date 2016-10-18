30296470a5b9bf37a29a017b3f5695d8cd733723
Ari Croock <aricroock@gmail.com>
2016-10-18 16:42:03 +0200

    Realtime kalman filter, using either threads or processes Kalman filter implementation is too slow, can't maintain 50 Hz

---
+++ MotionProcess.py
@@ -0,0 +1,64 @@
+from collections import deque
+from heave_sim.kalman import SinusoidalMotionKalmanFilter
+import numpy as np
+
+import multiprocessing as mp
+
+
+# Kalman filter parameters
+# number of sinusoidal modes expected in signal
+num_modes = 1
+Fs = 50
+dt = 1 / Fs
+R = 2e-5
+q = 0.00006
+N_window = 10 * Fs
+
+class MotionProcess(mp.Process):
+
+    def __init__(self, imu, motion_queue):
+        mp.Process.__init__(self)
+
+        self.imu = imu
+        self.motion_queue = motion_queue
+        self.kf = None
+
+        self.acc_buffer = deque([0] * N_window, maxlen=N_window)
+        self.num_samples = 0
+
+    def run(self):
+        if not self.imu.is_alive():
+            self.imu.start()
+
+        pos = {'x': 0., 'y': 0., 'z': 0., 'roll': 0., 'pitch': 0., 'yaw': 0.}
+
+        while True:
+            if not self.imu.data_ready():
+                continue
+
+            state = self.imu.get_state()
+            self.num_samples += 1
+            self.acc_buffer.append(-state[2])
+
+            pos['z'] = self.acc_buffer[-1]
+
+            if self.num_samples == N_window:
+                # can also use % (modulo) to re-initialise periodically
+                self.kf = SinusoidalMotionKalmanFilter(R, q, dt, num_modes, self.acc_buffer)
+
+            if self.num_samples >= N_window:
+                """
+                Apply Kalman filter to most recent sample
+                """
+                x = self.kf.step(self.acc_buffer[-1])
+                pos_sum = 100 * np.sum(x[0:-2:2], axis=0)
+                #pos['z'] = pos_sum
+
+            pos['roll'] = state[3]
+            pos['pitch'] = state[4]
+            pos['yaw'] = state[5]
+
+            # only send new pos info if the queue is not full, otherwise the
+            # queue will block
+            #if self.motion_queue.full():
+            self.motion_queue.put(pos)
--- SerialMock.py
+++ SerialMock.py
@@ -1,21 +1,26 @@
 import time
 import csv
+import numpy as np
 
 
 class SerialMock:
 
-    SAMPLE_RATE = 50
+    SAMPLE_RATE = 100
 
     def __init__(self, *args):
         self.prev_time = time.time()
 
         self.idx = 0
         with open('test_data.csv', 'r') as f:
+            i = 0
             reader = csv.reader(f, delimiter = "," )
             self.data = []
             for row in reader:
                 # = (float(i[3]),float(i[4]),float(i[5]))   #i[3] is roll, i[4] is pitch and [5] is yaw
                 self.data.append([float(d) for d in row])
+                self.data[-1][2] = np.sin(2*np.pi*1/SerialMock.SAMPLE_RATE*i)
+                print(self.data[-1][2])
+                i += 1
 
     def inWaiting(self):
         if time.time() - self.prev_time > 1/SerialMock.SAMPLE_RATE:
